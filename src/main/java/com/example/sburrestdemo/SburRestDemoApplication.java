baafe3b524bbb04a91bf1acb85adff4c9ecbaada
Simon Vargas <sjvargas01@gmail.com>
2022-07-31 11:49:23 -0400

    first commit

---
+++ src/main/java/com/example/sburrestdemo/SburRestDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.sburrestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SburRestDemoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SburRestDemoApplication.class, args);
+    }
+
+}
